ae2a5fcbc99f91da239df6ed01de92e2df8917f8
kimnguye <162928080+kimnguye@users.noreply.github.com>
2024-04-04 11:08:03 +0200

    Add files via upload

---
+++ Rush00/ex00/rush04.c
@@ -0,0 +1,96 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush04.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kimnguye <kimnguye@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/16 14:33:40 by kimnguye          #+#    #+#             */
+/*   Updated: 2024/03/16 18:33:00 by kimnguye         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c);
+
+void	first_line(int x)
+{
+	int	i;
+
+	i = 1;
+	ft_putchar('A');
+	while (i < x - 1)
+	{
+		ft_putchar('B');
+		i++;
+	}
+	if (x > 1)
+	{
+		ft_putchar('C');
+	}
+	ft_putchar('\n');
+}
+
+void	last_line(int x)
+{
+	int	i;
+
+	i = 1;
+	ft_putchar('C');
+	while (i < x - 1)
+	{
+		ft_putchar('B');
+		i++;
+	}
+	if (x > 1)
+	{
+		ft_putchar('A');
+	}
+	ft_putchar('\n');
+}
+
+void	inter_line(int x)
+{
+	int	i;
+
+	i = 1;
+	ft_putchar('B');
+	while (i < x - 1)
+	{
+		ft_putchar(' ');
+		i++;
+	}
+	if (x > 1)
+	{
+		ft_putchar('B');
+	}
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	int	j;
+
+	if (x <= 0 || y <= 0)
+	{
+		write (1, "x and y must be positive numbers\n", 42);
+	}
+	else
+	{
+		first_line(x);
+		if (y > 1)
+		{	
+			if (y > 2)
+			{
+				j = 2;
+				while (j < y)
+				{
+					inter_line(x);
+					j++;
+				}
+			}
+			last_line(x);
+		}
+	}
+}
